47342c696435c3b4ec92bc6dfe45b3fb2144799b
elite <elitetuber1122@gmail.com>
2024-12-24 13:30:48 +0600

    get Marathon details related apis and set query in All marathons apis

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -27,8 +27,21 @@ async function run() {
 
     // All marathons data
     app.get('/marathons', async (req, res) => {
-        const cursor = marathonsCollection.find();
+        const email = req.query.email;
+        let query = {};
+        if(email){
+            query = { creatorEmail: email }
+        }
+        const cursor = marathonsCollection.find(query);
         const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // Marathon details related apis
+    app.get('/marathons/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await marathonsCollection.findOne(query);
         res.send(result);
     })
 
@@ -38,6 +51,13 @@ async function run() {
         .limit(6)
         .toArray();
         res.send(marathonsInHome);
+    })
+
+    // New marathon create
+    app.post('/marathons', async (req, res) => {
+        const newMarathon = req.body;
+        const result = await marathonsCollection.insertOne(newMarathon);
+        res.send(result);
     })
 
     await client.connect();
